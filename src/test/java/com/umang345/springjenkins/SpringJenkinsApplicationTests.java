035dab52be43f187362dca4270be287b0db3e43f
Umang <umangagarwal345@gmail.com>
2022-06-14 16:06:41 +0530

    First commit

---
+++ src/test/java/com/umang345/springjenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.umang345.springjenkins;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@Test
+	void contextLoads()
+	{
+		logger.info("Test case executing...");
+		Assertions.assertEquals(true, true);
+	}
+
+}
